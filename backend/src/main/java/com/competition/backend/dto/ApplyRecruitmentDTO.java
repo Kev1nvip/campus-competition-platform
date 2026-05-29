b78ba2ca29c91f629f770cba6d0f05e117f01b3d
xm-csj <2491775991@qq.com>
2026-05-29 09:28:32 +0800

    feat:完成招募贴模块接口开发1

---
+++ backend/src/main/java/com/competition/backend/dto/ApplyRecruitmentDTO.java
@@ -0,0 +1,11 @@
+package com.competition.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class ApplyRecruitmentDTO {
+
+    private String introduction;
+
+    private String motivation;
+}
--- backend/src/main/java/com/competition/backend/repository/TeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.competition.backend.repository;
-
-import com.competition.backend.entity.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
